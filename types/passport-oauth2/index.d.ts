05a8c9db32a5c10871d497cbacacde644ad96d99
CallumJHays <callumjhays@gmail.com>
2019-11-30 00:15:37 +1000

    namespaces lead to shit code. Prove me wrong.

--- types/passport-oauth2/index.d.ts
+++ types/passport-oauth2/index.d.ts
@@ -22,8 +22,8 @@ export declare class OAuth2Strategy extends Strategy {
      */
     protected _oauth2: OAuth2;
 
-    constructor(options: oAuth2Strategy.StrategyOptions, verify: oAuth2Strategy.VerifyFunction);
-    constructor(options: oAuth2Strategy.StrategyOptionsWithRequest, verify: oAuth2Strategy.VerifyFunctionWithRequest);
+    constructor(options: StrategyOptions, verify: VerifyFunction);
+    constructor(options: StrategyOptionsWithRequest, verify: VerifyFunctionWithRequest);
 
     authenticate(req: Request, options?: any): void;
 
@@ -33,79 +33,74 @@ export declare class OAuth2Strategy extends Strategy {
     parseErrorResponse(body: any, status: number): Error | null;
 }
 
-declare namespace oAuth2Strategy {
-    interface Metadata {
-        authorizationURL: string;
-        tokenURL: string;
-        clientID: string;
-    }
+interface Metadata {
+    authorizationURL: string;
+    tokenURL: string;
+    clientID: string;
+}
 
-    type StateStoreStoreCallback = (err: Error | null, state: any) => void;
-    type StateStoreVerifyCallback = (err: Error, ok: boolean, state: any) => void;
+type StateStoreStoreCallback = (err: Error | null, state: any) => void;
+type StateStoreVerifyCallback = (err: Error, ok: boolean, state: any) => void;
 
-    interface StateStore {
-        store(req: Request, callback: StateStoreStoreCallback): void;
-        store(req: Request, meta: Metadata, callback: StateStoreStoreCallback): void;
+interface StateStore {
+    store(req: Request, callback: StateStoreStoreCallback): void;
+    store(req: Request, meta: Metadata, callback: StateStoreStoreCallback): void;
 
-        verify(req: Request, state: string, callback: StateStoreVerifyCallback): void;
-        verify(req: Request, state: string, meta: Metadata, callback: StateStoreVerifyCallback): void;
-    }
+    verify(req: Request, state: string, callback: StateStoreVerifyCallback): void;
+    verify(req: Request, state: string, meta: Metadata, callback: StateStoreVerifyCallback): void;
+}
 
-    type VerifyCallback = (err?: Error | null, user?: object, info?: object) => void;
+type VerifyCallback = (err?: Error | null, user?: object, info?: object) => void;
 
-    type VerifyFunction =
-        | ((accessToken: string, refreshToken: string, profile: any, verified: VerifyCallback) => void)
-        | ((accessToken: string, refreshToken: string, results: any, profile: any, verified: VerifyCallback) => void);
-    type VerifyFunctionWithRequest =
-        | ((req: Request, accessToken: string, refreshToken: string, profile: any, verified: VerifyCallback) => void)
-        | ((
-              req: Request,
-              accessToken: string,
-              refreshToken: string,
-              results: any,
-              profile: any,
-              verified: VerifyCallback,
-          ) => void);
+type VerifyFunction =
+    | ((accessToken: string, refreshToken: string, profile: any, verified: VerifyCallback) => void)
+    | ((accessToken: string, refreshToken: string, results: any, profile: any, verified: VerifyCallback) => void);
+type VerifyFunctionWithRequest =
+    | ((req: Request, accessToken: string, refreshToken: string, profile: any, verified: VerifyCallback) => void)
+    | ((
+          req: Request,
+          accessToken: string,
+          refreshToken: string,
+          results: any,
+          profile: any,
+          verified: VerifyCallback,
+      ) => void);
 
-    interface _StrategyOptionsBase {
-        authorizationURL: string;
-        tokenURL: string;
-        clientID: string;
-        clientSecret: string;
-        callbackURL?: string;
-        customHeaders?: OutgoingHttpHeaders;
-        scope?: string | string[];
-        scopeSeparator?: string;
-        sessionKey?: string;
-        store?: StateStore;
-        state?: any;
-    }
-    interface StrategyOptions extends _StrategyOptionsBase {
-        passReqToCallback?: false;
-    }
-    interface StrategyOptionsWithRequest extends _StrategyOptionsBase {
-        passReqToCallback: true;
-    }
+interface _StrategyOptionsBase {
+    authorizationURL: string;
+    tokenURL: string;
+    clientID: string;
+    clientSecret: string;
+    callbackURL?: string;
+    customHeaders?: OutgoingHttpHeaders;
+    scope?: string | string[];
+    scopeSeparator?: string;
+    sessionKey?: string;
+    store?: StateStore;
+    state?: any;
+}
+interface StrategyOptions extends _StrategyOptionsBase {
+    passReqToCallback?: false;
+}
+interface StrategyOptionsWithRequest extends _StrategyOptionsBase {
+    passReqToCallback: true;
+}
 
-    type Strategy = OAuth2Strategy;
-    const Strategy: typeof OAuth2Strategy;
+declare class TokenError extends Error {
+    constructor(message: string | undefined, code: string, uri?: string, status?: number);
+    code: string;
+    uri?: string;
+    status: number;
+}
 
-    class TokenError extends Error {
-        constructor(message: string | undefined, code: string, uri?: string, status?: number);
-        code: string;
-        uri?: string;
-        status: number;
-    }
+declare class AuthorizationError extends Error {
+    constructor(message: string | undefined, code: string, uri?: string, status?: number);
+    code: string;
+    uri?: string;
+    status: number;
+}
 
-    class AuthorizationError extends Error {
-        constructor(message: string | undefined, code: string, uri?: string, status?: number);
-        code: string;
-        uri?: string;
-        status: number;
-    }
-
-    class InternalOAuthError extends Error {
-        constructor(message: string, err: any);
-        oauthError: any;
-    }
+declare class InternalOAuthError extends Error {
+    constructor(message: string, err: any);
+    oauthError: any;
 }
--- types/passport-oauth2/passport-oauth2-tests.ts
+++ types/passport-oauth2/passport-oauth2-tests.ts
@@ -1,6 +1,16 @@
-import OAuth2Strategy = require('passport-oauth2');
-import { Strategy, StrategyOptions, StrategyOptionsWithRequest, VerifyCallback, AuthorizationError, TokenError,
-    InternalOAuthError, Metadata, StateStore, StateStoreStoreCallback, StateStoreVerifyCallback } from 'passport-oauth2';
+import {
+    OAuth2Strategy,
+    StrategyOptions,
+    StrategyOptionsWithRequest,
+    VerifyCallback,
+    AuthorizationError,
+    TokenError,
+    InternalOAuthError,
+    Metadata,
+    StateStore,
+    StateStoreStoreCallback,
+    StateStoreVerifyCallback,
+} from 'passport-oauth2';
 import { Strategy as PassportStrategy } from 'passport';
 import { Request } from 'express';
 
@@ -9,26 +19,43 @@ const strategyOptions1: StrategyOptions = {
     callbackURL: 'http://www.example.com/callback',
     clientID: 'dummy',
     clientSecret: 'secret',
-    tokenURL: 'http://www.example.com/token'
+    tokenURL: 'http://www.example.com/token',
 };
 
 function verifyFunction1(_accessToken: string, _refreshToken: string, _profile: any, verifyCallback: VerifyCallback) {
     verifyCallback(new Error('unimplemented'));
 }
 
-function verifyFunction2(_accessToken: string, _refreshToken: string, _results: any, _profile: any, verifyCallback: VerifyCallback) {
+function verifyFunction2(
+    _accessToken: string,
+    _refreshToken: string,
+    _results: any,
+    _profile: any,
+    verifyCallback: VerifyCallback,
+) {
     verifyCallback(new Error('unimplemented'));
 }
 
 const strategy1: OAuth2Strategy = new OAuth2Strategy(strategyOptions1, verifyFunction1);
 
-const strategy2: Strategy = new OAuth2Strategy(strategyOptions1, verifyFunction2);
-
-function verifyFunction3(_req: Request, _accessToken: string, _refreshToken: string, _profile: any, verifyCallback: VerifyCallback) {
+function verifyFunction3(
+    _req: Request,
+    _accessToken: string,
+    _refreshToken: string,
+    _profile: any,
+    verifyCallback: VerifyCallback,
+) {
     verifyCallback(undefined, { userid: '1' });
 }
 
-function verifyFunction4(_req: Request, _accessToken: string, _refreshToken: string, _results: any, _profile: any, verifyCallback: VerifyCallback) {
+function verifyFunction4(
+    _req: Request,
+    _accessToken: string,
+    _refreshToken: string,
+    _results: any,
+    _profile: any,
+    verifyCallback: VerifyCallback,
+) {
     verifyCallback(undefined, { userid: '1' });
 }
 
@@ -37,12 +64,10 @@ const strategyOptions2: StrategyOptionsWithRequest = {
     clientID: 'dummy',
     clientSecret: 'secret',
     tokenURL: 'http://www.example.com/token',
-    passReqToCallback: true
+    passReqToCallback: true,
 };
 
 const strategy3: PassportStrategy = new OAuth2Strategy(strategyOptions2, verifyFunction3);
-
-const strategy4: Strategy = new Strategy(strategyOptions2, verifyFunction4);
 
 const err1 = new AuthorizationError('Description', 'invalid_request', undefined);
 
@@ -61,12 +86,17 @@ class MyStrategy extends OAuth2Strategy {
 const metadata: Metadata = {
     authorizationURL: 'http://www.example.com/auth',
     clientID: 'dummy',
-    tokenURL: 'http://www.example.com/token'
+    tokenURL: 'http://www.example.com/token',
 };
 
 class MyStore implements StateStore {
     store(req: Request, meta: StateStoreStoreCallback | Metadata, callback?: StateStoreStoreCallback): void {}
-    verify(req: Request, state: string, meta: StateStoreVerifyCallback | Metadata, callback?: StateStoreVerifyCallback): void {}
+    verify(
+        req: Request,
+        state: string,
+        meta: StateStoreVerifyCallback | Metadata,
+        callback?: StateStoreVerifyCallback,
+    ): void {}
 }
 
 const myStore = new MyStore();
@@ -78,13 +108,11 @@ const strategyOptions3: StrategyOptions = {
     tokenURL: 'http://www.example.com/token',
     callbackURL: 'http://www.example.com/callback',
     customHeaders: {
-        'content-type': 'text/html'
+        'content-type': 'text/html',
     },
     scope: ['scope1', 'scope2'],
     scopeSeparator: ' ',
     sessionKey: 'oauth',
-    state: {id: 1},
-    store: myStore
+    state: { id: 1 },
+    store: myStore,
 };
-
-const strategy5: Strategy = new Strategy(strategyOptions3, verifyFunction2);
